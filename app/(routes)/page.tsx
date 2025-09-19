73e60b0bad519dea8feb119eeeb9ff1763908ef9
ana-maria.popescu@isa.utm.md <ana-maria.popescu@isa.utm.md>
2025-09-19 13:31:46 +0300

    Enviroment setup

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("3b6aa8d6-6faf-4f01-ba61-b0da55e837dc");
+
     return (
-        <div>
-            Hello Store
-        </div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            </div>
+        </Container>
     )
 }
 
